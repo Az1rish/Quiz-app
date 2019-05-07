c608435964a172af2036706911625e59960d1b70
Shane <shanemcneil19@gmail.com>
2019-05-06 22:55:26 -0700

    initial commit for index.js with pseudocode and an empty css stylesheet

---
+++ index.js
@@ -0,0 +1,28 @@
+function handleStartButton() {
+    // What happens when you click the start button and the quiz starts
+}
+
+function handleAnswerSubmit() {
+    // Should iterate through each question in sucession one at a time
+    // Should not be able to skip questions
+}
+
+function updateQuestionNumber() {
+    // self explanatory
+}
+
+function updateScore() {
+    // very similar to updateQuestionNumber()
+}
+
+function answerFeedback() {
+    // textual feedback about answer. If incorrect, correct answer will be given
+}
+
+function endQuiz() {
+    // should show overall score out of total asked
+}
+
+function restartQuiz() {
+    // once complete user should be able to retake quiz
+}
